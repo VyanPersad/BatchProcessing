8f53888f4e2a73f6e5fbe54dcbf9aec1550d862c
VyanPersad <vyanpersad@gmail.com>
2024-01-30 17:02:43 -0400

    Initial commit

---
+++ get_Pxs_frm_Img.py
@@ -0,0 +1,11 @@
+from getRandoPxVals import getPixels
+from SkinToneImg import skinTone
+
+#Select a pic form the Sample folder.
+#The output should print to the terminal.
+#The image skin1.png is produced to show the cropped region.
+path_to_pic = 'Sample_Pics/11.jpg'
+skinTone(path_to_pic)
+
+filename = 'skin1.png'
+getPixels(filename)
