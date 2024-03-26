f6ef099603816b01ae48447d1a4c64c671318146
VyanPersad <vyanpersad@gmail.com>
2024-03-25 22:20:28 -0400

    Added Gabor feature extraction.

--- extract.py
+++ extract.py
@@ -1,5 +1,6 @@
 from skimage.feature import graycomatrix
 from skimage.feature import graycoprops
+from skimage.filters import gabor
 import os
 import cv2
 import numpy as np
@@ -19,20 +20,39 @@ for file in os.listdir('CroppedImgs/'):
     symmetric = True
     normed = True
             
-    glcm = graycomatrix(gray_image, distances, angles, levels=levels, symmetric=symmetric, normed=normed)        
-        
-    cont = round(graycoprops(glcm, 'contrast').ravel()[0], 4)
-    diss = round(graycoprops(glcm, 'dissimilarity').ravel()[0], 4)
-    homo = round(graycoprops(glcm, 'homogeneity').ravel()[0], 4)
-    ener = round(graycoprops(glcm, 'energy').ravel()[0], 4)
-    corr = round(graycoprops(glcm, 'correlation').ravel()[0], 4)
-    asm = round(graycoprops(glcm, 'ASM').ravel()[0], 4)
-            
-    data = [{'Basename':base_name,'Contrast': cont, 
-             'Dissimilarity': diss, 'Homogeneity':homo, 
-             'Energy':ener, 'Correlation':corr, 'ASM':asm}]
+    glcm = graycomatrix(gray_image, distances, angles, levels=levels, symmetric=symmetric, normed=normed)                
 
-    header_names = ['Basename','Contrast', 'Dissimilarity','Homogeneity','Energy','Correlation','ASM']
+    cont = round(graycoprops(glcm, 'contrast').ravel()[0],4)
+    diss = round(graycoprops(glcm, 'dissimilarity').ravel()[0],4)
+    homo = round(graycoprops(glcm, 'homogeneity').ravel()[0],4)
+    ener = round(graycoprops(glcm, 'energy').ravel()[0],4)
+    corr = round(graycoprops(glcm, 'correlation').ravel()[0],4)
+    asm = round(graycoprops(glcm, 'ASM').ravel()[0],4)
+
+    frequencies = [0.1, 0.3, 0.5]
+    kernels = []
+
+    # Generate Gabor filter kernels
+    for frequency in frequencies:
+        for theta in angles:
+            kernel = np.real(gabor(gray_image, frequency, theta=theta))
+            kernels.append(np.mean(kernel))
+
+    # Convert the list of Gabor features to a numpy array
+    gabor_features = np.array(kernels)
+
+    data = [{'Basename':base_name,
+             'Contrast': cont, 'Dissimilarity': diss, 'Homogeneity':homo, 
+             'Energy':ener, 'Correlation':corr,'ASM':asm,
+             'Gabor1':round(gabor_features[0],4),'Gabor2':round(gabor_features[1],4),'Gabor3':round(gabor_features[2],4),
+             'Gabor4':round(gabor_features[3],4),'Gabor5':round(gabor_features[4],4),'Gabor6':round(gabor_features[5],4),
+             'Gabor7':round(gabor_features[6],4),'Gabor8':round(gabor_features[7],4),'Gabor9':round(gabor_features[8],4),
+             'Gabor10':round(gabor_features[9],4),'Gabor11':round(gabor_features[10],4),
+            }]
+
+    header_names = ['Basename','Contrast', 'Dissimilarity','Homogeneity','Energy','Correlation', 'ASM', 
+                    'Gabor1','Gabor2','Gabor3','Gabor4','Gabor5','Gabor6','Gabor7','Gabor8',
+                    'Gabor9','Gabor10','Gabor11']
 
     csv_file_path = 'data.csv'
     file_exists = os.path.exists(csv_file_path)
